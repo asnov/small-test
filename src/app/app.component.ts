05cb29f61e2bf2e9a564d874445ba9550866f5cd
Alex Novikov on mac-mini <alex@asnovikov.ru>
2018-09-17 03:45:25 -0300

    added ethers2 with "importHelpers": true;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 // recommended way as in https://github.com/ethers-io/ethers.js/issues/268#issuecomment-418159827
-import {ethers} from 'ethers';
+import {ethers} from 'ethers';    // Uncaught Error: Cannot find module 'tslib'
+// import {ethers} from 'ethers2';   // works fine
 
 
 @Component({

39fed914176d2c2314499ba5fa069c94887c9ba7
Alex Novikov on mac-mini <alex@asnovikov.ru>
2018-10-14 12:21:45 -0300

    Fixed at least one way to work;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 // recommended way as in https://github.com/ethers-io/ethers.js/issues/268#issuecomment-418159827
-import {ethers} from 'ethers';    // Uncaught Error: Cannot find module 'tslib'
+import { ethers } from 'ethers';    // Uncaught Error: Cannot find module 'tslib'
 // import {ethers} from 'ethers2';   // works fine
 
 
@@ -24,18 +24,29 @@ export class AppComponent {
     const ethProvider = new ethers.providers.EtherscanProvider();
 
     // EtherscanProvider.perform() -> fetchJson() -> XMLHttpRequest()
-    const perform = ethProvider.perform('', {});
-    console.log(`perform=`, perform);
+    // ethProvider.perform('log', {})
+    //   .then(perform => {
+    //     console.log(`perform=`, perform);
+    //   });
+
+    const address = '0xb2682160c482eB985EC9F3e364eEc0a904C44C23';
+    const startBlock = 3135808;
+    const endBlock = 5091477;
 
     // EtherscanProvider.getHistory() -> fetchJson() -> XMLHttpRequest()
-    const transactions = ethProvider.getHistory('');
-    console.log(`transactions=`, transactions);
+    ethProvider.getHistory(address, startBlock, endBlock)
+      .then(transactions => {
+        console.log(`transactions=`, transactions);
+      });
 
     const rpcProvider = new ethers.providers.JsonRpcProvider();
+    const hash = '0x2ddf6dd2ec23adf525dac59d7c9189b25b172d679aad951e59e232045f2c811f';
 
     // JsonRpcProvider.send() -> fetchJson() -> XMLHttpRequest()
-    const send = rpcProvider.send('', {});
-    console.log(`send=`, send);
+    rpcProvider.send('debug_traceTransaction', [hash])
+      .then(send => {
+        console.log(`send=`, send);
+      });
   }
 
 }
